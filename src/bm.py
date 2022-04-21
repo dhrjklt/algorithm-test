f293c11d7ecb1b01a700411b080c0605e686f650
Dhiraj Kalita <dhirajk@post.kek.jp>
2022-04-22 02:56:01 +0900

    BM Algo

--- src/bm.py
+++ src/bm.py
@@ -1,9 +1,13 @@
 from typing import Dict
 
+NO_OF_CHARS = 256
 
 def make_km_table(pattern: str) -> Dict[str, int]:
     table = dict()
-    raise Exception("TODO")
+    size= len(pattern)
+    table = [-1]*NO_OF_CHARS
+    for i in range(size):
+        table[ord(pattern[i])] = i
     return table
 
 
@@ -19,5 +23,16 @@ class Bm(object):
         return -1
 
     def search(self) -> int:
-        raise Exception("TODO")
+        m = len(self.pattern)
+        n = len(self.text)
+        s = 0
+        while(s <= n-m):
+            j = m-1
+            while j>=0 and self.pattern[j] == self.text[s+j]:
+                j -= 1
+            if j<0:
+                return s
+                s += (m-self.table[ord(self.text[s+m])] if s+m<n else 1)
+            else:
+                s += max(1, j-self.table[ord(self.text[s+j])])
         return -1
